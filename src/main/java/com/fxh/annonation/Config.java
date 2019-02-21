af9da49275dbd3f2fd3003e642f448a7de959403
FXH <1354004042@qq.com>
2019-02-21 11:51:32 +0800

    添加aop 代码

--- src/main/java/com/fxh/annonation/Config.java
+++ src/main/java/com/fxh/annonation/Config.java
@@ -1,6 +1,6 @@
 package com.fxh.annonation;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -10,9 +10,5 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class Config {
 
-   @Bean
-    public Boy getBoy(){
-        return new Boy();
-    }
 
 }
